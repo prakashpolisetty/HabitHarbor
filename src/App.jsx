106a71cece2be74679b3d0547051e7a7add2384f
prakashpolisetty <polisettygnanaprakash29348@gmail.com>
2025-10-16 11:27:41 +0530

    16octcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,11 +9,21 @@ import Progresstracker from './Components/Progresstracker'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() =>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  });
+  
+const addTask = (task) => {
+setTasks([...tasks,task]);
+}
+
   return (
     <>
       <h1>HABIT HARBOR</h1>
       <h4>A Task Managing app</h4>
-     <Taskform/>
+     <Taskform addTask={addTask}/>
      <TaskList/>
      <Progresstracker/>
      <button>Clear All Tasks</button>
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,27 +1,43 @@
-export default function Taskform() {
+import { useState } from "react"
+
+export default function Taskform({addTask}) {
+  const [task,setTask] = useState('');
+  const [priority , setPriority] = useState('');
+  const [select,setSelect] = useState('');
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    addTask({text: task,priority,select,completed:false});
+
+
+    setTask('');
+    setPriority('');
+    setSelect('');
+  }
     return(
         <>
+        <form onSubmit={handlesubmit}>
       <h1>This is Taskform</h1>
-      <input type="text" placeholder="Enter the task"/>
+      
+      <input type="text" placeholder="Enter the task" value={task} onChange={(e) => setTask(e.target.value)} />
      
 
       <label for="level">   Level</label>
-<select id="level" name="check">
-    <option value="normal">Select</option>
+<select value={priority} id="level" name="check"  onChange={(e) => setPriority(e.target.value)}>
   <option value="High">High</option>
-  <option value="Medium">Medium</option>
+  <option value="Medium" selected>Medium</option>
   <option value="Low">Low</option>
 </select>
 
 <label for="purpose">   Purpose</label>
-<select id="purpose" name="pur">
-    <option value="normal">Select</option>
+<select value={select} id="purpose" name="pur"  onChange={(e) => setSelect(e.target.value)}>
   <option value="work">Work</option>
-  <option value="personal">Personal Use</option>
+  <option value="personal Use" selected>Personal Use</option>
   <option value="general">General</option>
 </select>
-
+<h1>{task} {priority} {select}</h1>
  <button>Add Task</button>
+ </form>
         </>
     )
 }
